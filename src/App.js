e0afd5a72cbcbaf4395b63201794ef072fdc2d4a
tandavala <jose.tandavala@gmail.com>
2020-02-21 09:39:18 +0100

    ading validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from "react";
-import logo from "./logo.svg";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
 import "./App.css";
 
+const schema = Yup.object().shape({
+  firstName: Yup.string().required("O nome e obrigatorio"),
+  nacionalidade: Yup.string().required("A idade e obrigatorio")
+});
+
 function App() {
-  return <div className="App"></div>;
+  const { register, handleSubmit, errors } = useForm({
+    validationSchema: schema
+  });
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            First Name
+            <input type="text" name="firstName" ref={register} />
+          </label>
+          {errors.firstName && <p>{errors.firstName.message}</p>}
+
+          <label>
+            Age
+            <input type="text" name="nacionalidade" ref={register} />
+          </label>
+          {errors.nacionalidade && <p>{errors.nacionalidade.message}</p>}
+
+          <input type="submit" />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
